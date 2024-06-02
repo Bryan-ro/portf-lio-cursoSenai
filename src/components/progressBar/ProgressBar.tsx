961cff59be0e634cad22912554942df07ec2f85f
Bryan-ro <113953003+Bryan-ro@users.noreply.github.com>
2024-06-02 20:54:05 -0300

    teste

--- src/components/progressBar/ProgressBar.tsx
+++ src/components/progressBar/ProgressBar.tsx
@@ -7,11 +7,13 @@ type ProgressBarProps = {
 }
 
 export default function ProgressBar({ progressBarLoading, progressBarWidth, title }: ProgressBarProps) {
+
+    console.log(progressBarWidth);
     return (
         <div className="w-full p-1">
             <div className="flex justify-between">
-                <Paragraph className="text-sm font-bold text-blue-600">{title.toLocaleUpperCase()}</Paragraph>
-                <Paragraph className="text-sm font-bold text-blue-600">{progressBarWidth}%</Paragraph>
+                <Paragraph className="text-sm font-bold">{title.toLocaleUpperCase()}</Paragraph>
+                <Paragraph className="text-sm font-bold ">{progressBarWidth}%</Paragraph>
             </div>
             <div className="w-full rounded-full h-3.5 mb-4 bg-gray-700">
                 <div className={`bg-blue-600 h-3.5 rounded-full dark:bg-blue-500 ${!progressBarLoading ? "w-0" : `w-[${progressBarWidth}%]`} duration-1000`}></div>
